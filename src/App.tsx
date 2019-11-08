82f184c6fb12f8a9b8f4c6bfa97432814f37961e
msasinowski <maciej.sasinowski@acaisoft.com>
2019-11-08 14:20:27 +0100

    feat: add example usage of html marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AzureMap, IAzureMapOptions, AzureMapsProvider } from "react-azure-maps"
+import { AzureMap,AzureMapHtmlMarker, IAzureMapOptions, AzureMapsProvider } from "react-azure-maps"
 import { AuthenticationType} from 'azure-maps-control'
 
 import './App.css';
@@ -9,17 +9,24 @@ const App: React.FC = () => {
 
     authOptions: {
       authType: AuthenticationType.subscriptionKey,
-      subscriptionKey: "KEY",
+      subscriptionKey: "tTk1JVEaeNvDkxxnxHm9cYaCvqlOq1u-fXTvyXn2XkA",
     },
-    center: [-122.33, 47.6],
+    center: [-110, 45],
     zoom: 12,
     view:'Auto'
   };
+
+  const azureHtmlMapMarkerOptions = {
+    htmlContent: '<div class="pulseIcon"><</div>',
+    position: [-110, 45]
+  }
   return (
     <div>
       <div className="App">
         <AzureMapsProvider>
-          <AzureMap options={option} />
+          <AzureMap options={option} >
+            <AzureMapHtmlMarker options={azureHtmlMapMarkerOptions}></AzureMapHtmlMarker>
+            </AzureMap>
         </AzureMapsProvider>
         <AzureMapsProvider>
           <AzureMap options={option} />
