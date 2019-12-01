786babe5d52ed8b2b598908c17d5b3285ec12a32
Tomasz Bajda <tomasz.bajda96@gmail.com>
2019-12-01 18:15:17 +0100

    feat: new playground ui (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,15 @@
 import React from "react";
-import {
-  AzureMap,
-  AzureMapHtmlMarker,
-  IAzureMapOptions,
-  AzureMapsProvider,
-  IAzureMapHtmlMarkerEvent,
-  AzureMapDataSourceProvider,
-  AzureMapLayerProvider,
-  AzureMapFeature
-} from "react-azure-maps";
-import atlas, { AuthenticationType, data } from "azure-maps-control";
-
 import "./App.css";
-const xd = new data.Position(-100.01, 45.01);
 
+import Layout from "./Layout/Layout";
+
+import { BrowserRouter as Router } from "react-router-dom";
 const App: React.FC = () => {
-  const option: IAzureMapOptions = {
-    authOptions: {
-      authType: AuthenticationType.subscriptionKey,
-      subscriptionKey: ""
-    },
-    center: [-100.01, 45.01],
-    zoom: 12,
-    view: "Auto"
-  };
-
-  const onClick = () => {
-    console.log("ASD");
-  };
-
-  const azureHtmlMapMarkerOptions = {
-    htmlContent: '<div class="pulseIcon"></div>',
-    position: [-110, 45]
-  };
-
-  const eventToMarker: Array<IAzureMapHtmlMarkerEvent> = [
-    { eventName: "click", callback: onClick }
-  ];
-
   return (
     <div>
-      <div className="App">
-        <AzureMapsProvider>
-          <AzureMap options={option}>
-            <AzureMapDataSourceProvider>
-              <AzureMapLayerProvider></AzureMapLayerProvider>
-              <AzureMapFeature
-                type="Point"
-                coordinate={xd}
-                properties={{
-                  title: "Microsoft",
-                  icon: "pin-round-blue"
-                }}
-              ></AzureMapFeature>
-            </AzureMapDataSourceProvider>
-            <AzureMapHtmlMarker
-              options={azureHtmlMapMarkerOptions}
-              events={eventToMarker}
-            />
-          </AzureMap>
-        </AzureMapsProvider>
-      </div>
+      <Router>
+        <Layout />
+      </Router>
     </div>
   );
 };
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import PageContent from "./PageContent";
+import logo from "../Assets/Melly_logo.png";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="Layout">
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <PageContent></PageContent>
+    </div>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <h4>React Azure Maps Playground</h4>
+    </header>
+  );
+};
+
+export default Layout;
