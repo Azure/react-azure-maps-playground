35e0baabeb5013298c337c95d4e7e566294e54f6
Patryk Średnicki <41010528+srednicki95@users.noreply.github.com>
2020-02-06 10:09:43 +0100

    feat: Layer events (#8)
    * feat: layer feature injected
    
    * feat: Layer events
    
    * feat: Layer lifecycle events
    
    * fix: touch-ups
    
    * feat: dataSource events

--- src/examples/MyDump/MyDump.tsx
+++ src/examples/MyDump/MyDump.tsx
@@ -15,6 +15,10 @@ const point1 = new data.Position(-100.01, 45.01);
 const point2 = new data.Position(-120.2, 45.1);
 const point3 = new data.Position(-120.2, 50.1);
 
+function clusterClicked(e: any) {
+    console.log('clusterClicked', e)
+}
+
 const onClick = () => {
     console.log("ASD");
 };
@@ -60,7 +64,7 @@ const MyDump: React.FC = () => {
     const [dump, setDump] = useState('START');
     const [markers, setMarkers] = useState([point1, point2, point3]);
     const [htmlMarkers, setHtmlMarkers] = useState([point1]);
-    const [markersLayer, setMarkersLayer] = useState<IAzureMapLayerType>('HeatLayer');
+    const [markersLayer, setMarkersLayer] = useState<IAzureMapLayerType>('SymbolLayer');
 
     const option: IAzureMapOptions = useMemo(() => {
         return {
@@ -124,15 +128,31 @@ const MyDump: React.FC = () => {
                 <div style={styles.button} onClick={addRandomMarker}> MARKER POINT</div>
                 <div style={styles.button} onClick={addRandomHTMLMarker}> HTML MARKER</div>
                 <div style={styles.button} onClick={removeAllMarkers}> REMOVE ALL</div>
-                <div style={styles.button} onClick={() => setMarkersLayer('SymbolLayer')}> SET POINT </div>
-                <div style={styles.button} onClick={() => setMarkersLayer('HeatLayer')}> SET HEAT </div>
+                <div style={styles.button} onClick={() => setMarkersLayer('SymbolLayer')}> SET POINT</div>
+                <div style={styles.button} onClick={() => setMarkersLayer('HeatLayer')}> SET HEAT</div>
             </div>
             <div>
                 <AzureMapsProvider>
                     <AzureMap options={option}>
-                        <AzureMapDataSourceProvider id={'myDump AzureMapDataSourceProvider'}>
-                            <AzureMapLayerProvider id={'myDump AzureMapLayerProvider'} options={{}}
-                                                   type={markersLayer}></AzureMapLayerProvider>
+                        <AzureMapDataSourceProvider events={{
+                            'dataadded': (e: any) => {
+                                console.log('Data on source added', e)
+                            }
+                        }} id={'myDump AzureMapDataSourceProvider'}>
+                            <AzureMapLayerProvider
+                                id={'myDump AzureMapLayerProvider'}
+                                options={{}}
+                                events={{
+                                    'click': clusterClicked,
+                                    'dbclick': clusterClicked
+                                }}
+                                lifecycleEvents={{
+                                    'layeradded': () => {
+                                        console.log("LAYER ADDED TO MAP")
+                                    },
+                                }}
+                                type={markersLayer}
+                            ></AzureMapLayerProvider>
                             {memoizedMarkerRender}
                             {memoizedHtmlMarkerRender}
                         </AzureMapDataSourceProvider>
