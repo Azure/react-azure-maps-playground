14f4e2e9892631292e32421d60ec1c9133929d0a
Patryk Średnicki <41010528+srednicki95@users.noreply.github.com>
2020-02-14 07:51:26 +0100

    feat: default map update as simple as possible (#21)

--- src/examples/DefaultMap.tsx
+++ src/examples/DefaultMap.tsx
@@ -1,76 +1,22 @@
-import React, { useMemo } from 'react'
-import {
-  AzureMap,
-  AzureMapDataSourceProvider,
-  AzureMapFeature,
-  AzureMapHtmlMarker,
-  AzureMapLayerProvider,
-  AzureMapsProvider,
-  IAzureMapHtmlMarkerEvent,
-  IAzureMapOptions
-} from 'react-azure-maps'
-import { AuthenticationType, data } from 'azure-maps-control'
-import { key } from '../key'
+import React from 'react'
+import {AzureMap, AzureMapsProvider, IAzureMapOptions} from 'react-azure-maps'
+import {AuthenticationType} from 'azure-maps-control'
+import {key} from '../key'
 
-const onClick = () => {
-  console.log('CLICK')
-}
-
-const azureHtmlMapMarkerOptions = {
-  position: [-110, 45]
-}
-
-const eventToMarker: Array<IAzureMapHtmlMarkerEvent> = [
-  { eventName: 'click', callback: onClick }
-]
-
-
-const DefaultMap: React.FC = () => {
-  const position = new data.Position(-100.01, 45.01)
-
-  const option: IAzureMapOptions = useMemo(() => {
-    return {
-      authOptions: {
+const option: IAzureMapOptions = {
+    authOptions: {
         authType: AuthenticationType.subscriptionKey,
         subscriptionKey: key
-      },
-      center: [-100.01, 45.01],
-      zoom: 12,
-      view: 'Auto'
-    }
-  }, [])
-
-  return (
-    <div style={{
-        height: '300px'
-    }}>
-      <AzureMapsProvider>
-        <AzureMap options={option}>
-          <AzureMapDataSourceProvider id={'defaultMap DataSourceProvider'}>
-            <AzureMapLayerProvider
-              id={'defaultMap LayerProvider'}
-              options={{}}
-              type={'SymbolLayer'}
-            ></AzureMapLayerProvider>
-            <AzureMapFeature
-              id={'default MapFeature'}
-              type="Point"
-              coordinate={position}
-              properties={{
-                title: 'Microsoft',
-                icon: 'pin-round-blue'
-              }}
-            ></AzureMapFeature>
-          </AzureMapDataSourceProvider>
-          <AzureMapHtmlMarker
-            markerContent={<div className="pulseIcon"></div>}
-            options={azureHtmlMapMarkerOptions}
-            events={eventToMarker}
-          />
-        </AzureMap>
-      </AzureMapsProvider>
-    </div>
-  )
+    },
 }
+
+const DefaultMap: React.FC = () => (
+    <div style={{height: '300px'}}>
+        <AzureMapsProvider>
+            <AzureMap options={option}>
+            </AzureMap>
+        </AzureMapsProvider>
+    </div>
+)
 
 export default DefaultMap
