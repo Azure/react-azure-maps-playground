48ca11614816a791c6161593216fed24d046d56e
psrednicki <patryk.srednicki@acaisoft.com>
2020-10-08 17:55:35 +0200

    feat: add prettier and eslint

--- src/examples/ControlExample.tsx
+++ src/examples/ControlExample.tsx
@@ -1,70 +1,68 @@
-import React from "react";
+import React from 'react';
 import {
-  AzureMap,
-  AzureMapDataSourceProvider,
-  AzureMapLayerProvider,
-  AzureMapsProvider,
-  IAzureMapControls,
-  IAzureMapOptions
-} from "react-azure-maps";
-import { AuthenticationType, ControlOptions } from "azure-maps-control";
-import { key } from "../key";
-import Typography from "@material-ui/core/Typography";
-import { wrapperStyles } from "./RouteExample";
-import { calculateLineEndPoints, lineData } from "./mapHelper";
-import Description from "../Layout/Description";
+    AzureMap,
+    AzureMapDataSourceProvider,
+    AzureMapLayerProvider,
+    AzureMapsProvider,
+    IAzureMapControls,
+    IAzureMapOptions,
+} from 'react-azure-maps';
+import { AuthenticationType, ControlOptions } from 'azure-maps-control';
+import { key } from '../key';
+import Typography from '@material-ui/core/Typography';
+import { wrapperStyles } from './RouteExample';
+import { calculateLineEndPoints, lineData } from './mapHelper';
+import Description from '../Layout/Description';
 
 const option: IAzureMapOptions = {
-  authOptions: {
-    authType: AuthenticationType.subscriptionKey,
-    subscriptionKey: key
-  },
-  center: [-100.12, 44.63],
-  zoom: 3,
-  view: "Auto"
+    authOptions: {
+        authType: AuthenticationType.subscriptionKey,
+        subscriptionKey: key,
+    },
+    center: [-100.12, 44.63],
+    zoom: 3,
+    view: 'Auto',
 };
 
 const controls: [IAzureMapControls] = [
-  {
-    controlName: "StyleControl",
-    controlOptions: { mapStyles: "all" },
-    options: { position: "top-right" } as ControlOptions
-  }
+    {
+        controlName: 'StyleControl',
+        controlOptions: { mapStyles: 'all' },
+        options: { position: 'top-right' } as ControlOptions,
+    },
 ];
 
 const ControlExample: React.FC = () => {
-  return (
-    <div style={wrapperStyles.map}>
-      <Typography gutterBottom variant="h5">
-        Map Control Style
-      </Typography>
-      <Description>
-        This sample shows how to add the map style picker control to the map.
-      </Description>
-      <div style={wrapperStyles.map}>
-        <AzureMapsProvider>
-          <AzureMap options={option} controls={controls}>
-            <AzureMapDataSourceProvider
-              events={{
-                dataadded: (e: any) => {
-                  console.log("Data on source added", e);
-                }
-              }}
-              id={"controlExample AzureMapDataSourceProvider"}
-              options={{}}
-            >
-              <AzureMapLayerProvider
-                id={"controlExample AzureMapLayerProvider"}
-                options={{}}
-                type={"SymbolLayer"}
-              ></AzureMapLayerProvider>
-              {calculateLineEndPoints(lineData)}
-            </AzureMapDataSourceProvider>
-          </AzureMap>
-        </AzureMapsProvider>
-      </div>
-    </div>
-  );
+    return (
+        <div style={wrapperStyles.map}>
+            <Typography gutterBottom variant="h5">
+                Map Control Style
+            </Typography>
+            <Description>This sample shows how to add the map style picker control to the map.</Description>
+            <div style={wrapperStyles.map}>
+                <AzureMapsProvider>
+                    <AzureMap options={option} controls={controls}>
+                        <AzureMapDataSourceProvider
+                            events={{
+                                dataadded: (e: any) => {
+                                    console.log('Data on source added', e);
+                                },
+                            }}
+                            id={'controlExample AzureMapDataSourceProvider'}
+                            options={{}}
+                        >
+                            <AzureMapLayerProvider
+                                id={'controlExample AzureMapLayerProvider'}
+                                options={{}}
+                                type={'SymbolLayer'}
+                            ></AzureMapLayerProvider>
+                            {calculateLineEndPoints(lineData)}
+                        </AzureMapDataSourceProvider>
+                    </AzureMap>
+                </AzureMapsProvider>
+            </div>
+        </div>
+    );
 };
 
 export default ControlExample;
--- src/examples/examplesList.tsx
+++ src/examples/examplesList.tsx
@@ -1,126 +1,126 @@
-import DefaultMap from './DefaultMap'
-import AzureLayer from './AzureLayer'
-import PopupExample from './PopupExample'
-import RouteExample from './RouteExample'
-import ArrowLineExample from './ArrowLinesExample'
-import ControlExample from './ControlExample'
-import ChronoplethMap from './ChoroplethMap'
-import MarkersExampleWrapper from "./MarkersExample/MarkersExampleWrapper";
-import BringDataControl from "./CustomControl/BringDataControl";
-import BubbleLayer from "./BubbleLayer";
-import IssMapPosition from "./Live/issPosition";
-import MultiPOint from './MultiplePointWithPopup'
-import ShapeExample from "./Shape/Shape";
-import COVID from "./Live/COVID-19";
-import HTMLCustomMarkerLayer from "./CustomLayer/HTMLCustomMarkerLayer";
-import AsyncPopup from './AsyncPopupExample'
-import PolygonExample from "./PolygonExample";
-import TrafficOptions from "./TrafficOptions";
-import TrafficOptionsExample from "./TrafficOptions";
-import ChangeOptionsWrapper from "./Options/ChangeOptionsWrapper";
+import DefaultMap from './DefaultMap';
+import AzureLayer from './AzureLayer';
+import PopupExample from './PopupExample';
+import RouteExample from './RouteExample';
+import ArrowLineExample from './ArrowLinesExample';
+import ControlExample from './ControlExample';
+import ChronoplethMap from './ChoroplethMap';
+import MarkersExampleWrapper from './MarkersExample/MarkersExampleWrapper';
+import BringDataControl from './CustomControl/BringDataControl';
+import BubbleLayer from './BubbleLayer';
+import IssMapPosition from './Live/issPosition';
+import MultiPOint from './MultiplePointWithPopup';
+import ShapeExample from './Shape/Shape';
+import COVID from './Live/COVID-19';
+import HTMLCustomMarkerLayer from './CustomLayer/HTMLCustomMarkerLayer';
+import AsyncPopup from './AsyncPopupExample';
+import PolygonExample from './PolygonExample';
+import TrafficOptions from './TrafficOptions';
+import TrafficOptionsExample from './TrafficOptions';
+import ChangeOptionsWrapper from './Options/ChangeOptionsWrapper';
 
 export type MapExampleItem = {
-  name: string
-  component: React.FC
-  path: string
-  exact?: boolean
-}
+    name: string;
+    component: React.FC;
+    path: string;
+    exact?: boolean;
+};
 
 export const examplesList: MapExampleItem[] = [
-  {
-    name: 'Default map',
-    component: DefaultMap,
-    path: '/',
-    exact: true
-  },
-  {
-    name: 'Layers',
-    component: AzureLayer,
-    path: '/layers'
-  },
-  {
-    name: 'Bubble layer',
-    component: BubbleLayer,
-    path: '/bubble-layer'
-  },
-  {
-    name: 'Markers',
-    component: MarkersExampleWrapper,
-    path: '/markers-example'
-  },
-  {
-    name: 'Shape',
-    component: ShapeExample,
-    path: '/shape-example'
-  },
-  {
-    name: 'Popup',
-    component: PopupExample,
-    path: '/popup-example'
-  },
-  {
-    name: 'Popup with multiple point on hover',
-    component: MultiPOint,
-    path: '/popup-multi-markers'
-  },
-  {
-    name: 'Route Example',
-    component: RouteExample,
-    path: '/route-example'
-  },
-  {
-    name: 'Arrow Lines & Image Sprite',
-    component: ArrowLineExample,
-    path: '/arrowLine-example'
-  },
-  {
-    name: 'Style Controls',
-    component: ControlExample,
-    path: '/controls'
-  },
-  {
-    name: 'Chronopleth map',
-    component: ChronoplethMap,
-    path: '/chrono'
-  },
-  {
-    name: 'Custom Control',
-    component: BringDataControl,
-    path: '/bring-data-control'
-  },
-  {
-    name: 'ASync change custom pin position',
-    component: IssMapPosition,
-    path: '/iss-position'
-  },
-  // {
-  //   name: 'COVID-19',
-  //   component: COVID,
-  //   path: '/COVID-19'
-  // },
-  {
-    name: 'HTML Markers Layer',
-    component: HTMLCustomMarkerLayer,
-    path: '/html-markers-layer'
-  },
-  {
-    name: 'Async Popup',
-    component: AsyncPopup,
-    path: '/async-popup'
-  },
-  {
-    name: 'Polygon Example',
-    component: PolygonExample,
-    path: '/polygon-example'
-  },
-  {
-    name: 'Traffic Options',
-    component: TrafficOptionsExample,
-    path: '/traffic-options'
-  },
-  {
-    name: 'Change Options In Child',
-    component: ChangeOptionsWrapper,
-    path: '/change-options-child'
-  }
-]
+    {
+        name: 'Default map',
+        component: DefaultMap,
+        path: '/',
+        exact: true,
+    },
+    {
+        name: 'Layers',
+        component: AzureLayer,
+        path: '/layers',
+    },
+    {
+        name: 'Bubble layer',
+        component: BubbleLayer,
+        path: '/bubble-layer',
+    },
+    {
+        name: 'Markers',
+        component: MarkersExampleWrapper,
+        path: '/markers-example',
+    },
+    {
+        name: 'Shape',
+        component: ShapeExample,
+        path: '/shape-example',
+    },
+    {
+        name: 'Popup',
+        component: PopupExample,
+        path: '/popup-example',
+    },
+    {
+        name: 'Popup with multiple point on hover',
+        component: MultiPOint,
+        path: '/popup-multi-markers',
+    },
+    {
+        name: 'Route Example',
+        component: RouteExample,
+        path: '/route-example',
+    },
+    {
+        name: 'Arrow Lines & Image Sprite',
+        component: ArrowLineExample,
+        path: '/arrowLine-example',
+    },
+    {
+        name: 'Style Controls',
+        component: ControlExample,
+        path: '/controls',
+    },
+    {
+        name: 'Chronopleth map',
+        component: ChronoplethMap,
+        path: '/chrono',
+    },
+    {
+        name: 'Custom Control',
+        component: BringDataControl,
+        path: '/bring-data-control',
+    },
+    {
+        name: 'ASync change custom pin position',
+        component: IssMapPosition,
+        path: '/iss-position',
+    },
+    // {
+    //   name: 'COVID-19',
+    //   component: COVID,
+    //   path: '/COVID-19'
+    // },
+    {
+        name: 'HTML Markers Layer',
+        component: HTMLCustomMarkerLayer,
+        path: '/html-markers-layer',
+    },
+    {
+        name: 'Async Popup',
+        component: AsyncPopup,
+        path: '/async-popup',
+    },
+    {
+        name: 'Polygon Example',
+        component: PolygonExample,
+        path: '/polygon-example',
+    },
+    {
+        name: 'Traffic Options',
+        component: TrafficOptionsExample,
+        path: '/traffic-options',
+    },
+    {
+        name: 'Change Options In Child',
+        component: ChangeOptionsWrapper,
+        path: '/change-options-child',
+    },
+];
